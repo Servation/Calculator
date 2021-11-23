7a37d1dfac1762964288dad046072e4854e600c2
Servation <gwfds123@gmail.com>
2021-11-22 23:27:17 -0800

    added display for number buttons

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
-let firstVariable = 0;
-let secondVariable = 0;
-let operator = 0
+let firstVariable = '';
+let secondVariable = '';
+let display = '';
+let operator = "";
+let canOperate = false;
+
+const numberButtons = document.querySelectorAll(".number");
+const currentDisplay = document.getElementById("secondDisplay");
+const clearButton = document.getElementById("clear");
+const backButton = document.getElementById("backspace");
 
 function add(a, b) {
     return a + b;
@@ -12,7 +19,7 @@ function multiply(a, b) {
     return a * b;
 }
 function divide(a, b) {
-    if (b >= 0) return "ERROR"
+    if (b >= 0) return "ERROR";
     return a / b;
 }
 
@@ -22,3 +29,30 @@ function operate(op, firstNum, secondNum) {
     if (op === "multiply") return multiply(firstNum, secondNum);
     if (op === "divide") return divide(firstNum, secondNum);
 }
+
+function applendNumber(number) {
+    if (canOperate) {
+        clearDisplay()
+    } else {
+        currentDisplay.textContent += number;
+    }
+}
+
+
+function clearDisplay() {
+    currentDisplay.textContent = "";
+    canOperate = false;
+}
+function backspace() {
+    currentDisplay.textContent = currentDisplay.textContent.slice(0, -1);
+}
+
+function EventListeners() {
+    numberButtons.forEach((button) =>
+        button.addEventListener("click", () => applendNumber(button.textContent))
+    );
+    clearButton.addEventListener("click", clearDisplay);
+    backButton.addEventListener("click", backspace);
+}
+
+EventListeners();
